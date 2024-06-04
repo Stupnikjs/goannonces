eaa2bb200b0080a16e4d88b8d29846814bf74025
stupnikk <andre.lacassagne82@gmail.com>
2024-06-04 20:42:27 +0200

    ok

--- gcp.go
+++ gcp.go
@@ -1,5 +1,14 @@
 package main
 
-func (app *application) ConnectToGcp() {
+import (
+	_ "cloud.google.com/go/storage"
+	"google.golang.org/api/option"
+	_ "google.golang.org/api/option"
+)
 
+func (app *application) ConnectToGcp() {
+	// read json credentials file
+	creds := make([]byte, 1000)
+	options := option.WithCredentialsJSON(creds)
+	print(options)
 }
