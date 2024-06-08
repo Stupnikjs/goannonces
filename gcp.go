16ca20d723a86eb98cf4d4add9cd1f8fec1dfebb
Stupnikjs <106406099+Stupnikjs@users.noreply.github.com>
2024-06-08 16:11:07 +0200

    Update gcp.go

--- gcp.go
+++ gcp.go
@@ -25,9 +25,38 @@ func CreateBucket(client *storage.Client, bucket *storage.BucketHandle, ctx cont
 
 }
 
-func (app *application) LoadToBucket([]byte data, ) error {
+func (app *application) LoadToBucket(fileName string, []byte data) error {
 
 
+	
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+		client, err := storage.NewClient(ctx)
+
+
+
+		buck := client.Bucket(app.BucketName)
+
+		// Check if bucket already created
+		err = CreateBucket(client, buck, ctx)
+
+		if err != nil {
+			http.Error(w, fmt.Sprintf("err occurs %s", err), http.StatusBadRequest)
+		}
+		defer client.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+
+			obj := buck.Object(fileName)
+
+			writer := obj.NewWriter(ctx)
+   writer.Write(buf)
+			defer writer.Close()
+			if err != nil {
+				fmt.Println(err)
+			}
 
 
 return nil 
