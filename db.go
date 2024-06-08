7c6d75d680683759c63eadecebbeb8f14d178974
Stupnikjs <106406099+Stupnikjs@users.noreply.github.com>
2024-06-08 16:25:30 +0200

    Update db.go

--- db.go
+++ db.go
@@ -1,9 +1,48 @@
 package main
 
-func ConnectToDb() {
 
+
+func openDB() (*sql.DB, error) {
+
+	cleanup, err := pgxv4.RegisterDriver("cloudsql-postgres", cloudsqlconn.WithCredentialsFile("credentials.json"))
+	if err != nil {
+		fmt.Println(err)
+	}
+	// call cleanup when you're done with the database connection
+	defer cleanup()
+
+	if err != nil {
+		fmt.Println(err)
+
+	}
+
+	listDir()
+	// Call cleanup when you're done with the database connection
+
+	db, err := sql.Open(
+		"cloudsql-postgres",
+		fmt.Sprintf("host=%s user=postgres password=%s dbname=postgres sslmode=disable", os.Getenv("SQL_HOST"), os.Getenv("SQL_PASSWORD")))
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Print(err)
+		return nil, err
+	}
+
+	return db, nil
 }
 
-func GetAllTracks() []Track {
-	return []Track{}
+func (app *application) connectToDB() (*sql.DB, error) {
+
+	connection, err := openDB()
+
+	if err != nil {
+		return nil, err
+	}
+	log.Println("Connected to Postgres!")
+	return connection, nil
 }
