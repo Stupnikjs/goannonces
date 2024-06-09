c821a913df9118608a444ae5d643b839a5ef7a24
stupnikk <andre.lacassagne82@gmail.com>
2024-06-09 21:34:01 +0200

    ok

--- static/js/loader.js
+++ static/js/loader.js
@@ -2,19 +2,12 @@ const inputSubmit = document.querySelector("#submitFileNum")
 
 let fileNumber = 1
 
-let form = document.querySelector("form")
-form.style.display = "flex"
-form.style.flexDirection = "column"
-
-
-inputSubmit.addEventListener("click", (e) => {
-    e.preventDefault()
-    console.log(e)
-    console.log("michel")
-    fileNumber = document.querySelector("#selectFileNum").value
+inputSubmit.addEventListener("change", (e) => {
+    fileNumber = document.querySelector("selectFileNum").value
     
-    for (let i=0; i < fileNumber; i++){
+    let form = document.querySelector("form")
 
+    for (let i=0; i < fileNumber; i++){
         let fileInput = document.createElement("input")
         fileInput.type = "file"
         fileInput.name = "file"+parseInt(i)
