f1b950c69ce5b7388af92a47e0026fd174a6886d
Stupnikjs <106406099+Stupnikjs@users.noreply.github.com>
2024-06-10 21:33:50 +0200

    Update gcp_test.go

--- gcp_test.go
+++ gcp_test.go
@@ -1,1 +1,35 @@
 package main 
+
+
+import (
+"testing" 
+"os"
+)
+
+func TestLoadToBucket(t testing.T)  {
+    
+}
+
+func TestCreateBucket(t testing.T)  {
+
+}
+
+
+func TestGetBucketObject(t testing.T){
+
+  
+   mockFile , err := os.Create("test.txt") 
+   mockFile.Write([]byte("this is test files content"))
+   defer mockFile.Close()
+   defer os.Remove(mockFile)
+   
+    // Push mock object to bucket 
+
+   
+   
+   // Call get bucket method 
+   if expectedObjectName != actualObjectName {
+  t.Errorf("expected %s as object name but got %s", expectedObjectName, actualObjectName)
+}
+
+} 
