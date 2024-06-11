fb9890c90a0d5eaa030981507b00acbdad89c498
Stupnikjs <106406099+Stupnikjs@users.noreply.github.com>
2024-06-11 20:07:04 +0200

    Update utils.go

--- utils.go
+++ utils.go
@@ -1,5 +1,12 @@
 package main
 
-func IsInSlice[T any](str T, arr []T) {
-
+func IsInSlice[T any](str T, arr []T) bool {
+for _,s := range arr {
+  if str == s {
+  return true 
 }
+  
+}
+return false 
+}
+
