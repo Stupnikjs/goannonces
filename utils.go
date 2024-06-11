0d06b1399ee99585f3ee2c709c07170a8d20f01d
Stupnikjs <106406099+Stupnikjs@users.noreply.github.com>
2024-06-11 20:12:11 +0200

    Update utils.go

--- utils.go
+++ utils.go
@@ -1,5 +1,11 @@
 package main
 
+
+import (
+"io"
+"bufio"
+"log"
+)
 func IsInSlice[T any](str T, arr []T) bool {
 for _,s := range arr {
   if str == s {
@@ -10,3 +16,23 @@ for _,s := range arr {
 return false 
 }
 
+
+func ByteFromMegaFile(file io.Reader) ([]byte, error) {
+
+        reader := bufio.NewReader(file)
+
+        finalByteArr := make([]byte, 0, 2048*1000)
+
+        for {
+                soloByte, err := reader.ReadByte()
+                if err != nil {
+                        log.Println(err)
+                        break
+                }
+
+                finalByteArr = append(finalByteArr, soloByte)
+        }
+
+        return finalByteArr, nil
+
+}
