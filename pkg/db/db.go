64e4dcd0d6600403dc084508f133cff9eeb3b3c3
Stupnikjs <106406099+Stupnikjs@users.noreply.github.com>
2024-06-13 15:40:45 +0200

    Update db.go

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -25,49 +25,6 @@ type PostgresRepo struct {
 	DB *sql.DB
 }
 
-func openDB() (*sql.DB, error) {
-
-	cleanup, err := pgxv4.RegisterDriver("cloudsql-postgres", cloudsqlconn.WithCredentialsFile("credentials.json"))
-	if err != nil {
-		fmt.Println(err)
-	}
-	// call cleanup when you're done with the database connection
-	defer cleanup()
-
-	if err != nil {
-		fmt.Println(err)
-
-	}
-
-	// Call cleanup when you're done with the database connection
-
-	db, err := sql.Open(
-		"cloudsql-postgres",
-		fmt.Sprintf("host=%s user=postgres password=%s dbname=postgres sslmode=disable", os.Getenv("SQL_HOST"), os.Getenv("SQL_PASSWORD")))
-
-	if err != nil {
-		return nil, err
-	}
-
-	err = db.Ping()
-	if err != nil {
-		fmt.Print(err)
-		return nil, err
-	}
-
-	return db, nil
-}
-
-func (rep *PostgresRepo) connectToDB() (*sql.DB, error) {
-
-	connection, err := openDB()
-
-	if err != nil {
-		return nil, err
-	}
-	log.Println("Connected to Postgres!")
-	return connection, nil
-}
 
 func (rep *PostgresRepo) PushTrackToSQL(track Track) error {
 	ctx, cancel := context.WithCancel(context.Background())
