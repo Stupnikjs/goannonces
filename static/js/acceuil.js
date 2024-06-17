1eae33650ec4bcf79aac381050530bee7495062b
stupnikk <andre.lacassagne82@gmail.com>
2024-06-17 09:40:05 +0200

    ok

--- static/js/acceuil.js
+++ static/js/acceuil.js
@@ -1,7 +1,10 @@
 let cards = document.querySelectorAll(".audiocard")
 
 for ( let i = 0; i < cards.length; i++ ){
-    let button = cards[i].querySelector("button")
+    let button = cards[i].querySelector(".nameBtn")
+    let tagBtn = cards[i].querySelector(".tagBtn")
+    let selectedBtn = cards[i].querySelector(".selectedBtn")
+
     // create toggle button (todo) 
     button.addEventListener("click", (e) => {
         e.preventDefault()
@@ -12,9 +15,45 @@ for ( let i = 0; i < cards.length; i++ ){
             audio.src = button.getAttribute("data-url") 
             audio.controls = true
             audio.preload = "auto"
-            console.log(button.getAttribute("data-url"))
             cards[i].appendChild(audio)
         }
        
     })
+    tagBtn.addEventListener("click", (e) => {
+        e.preventDefault()
+        let selected = tagBtn.getAttribute("selected")
+        if (selected == "false") {
+            selected = null
+        }
+        if (!selected){
+            tagBtn.setAttribute("selected", "true")
+            let hiddenTagDiv = cards[i].querySelector(".hiddenTagDiv")
+            hiddenTagDiv.classList.remove("hiddenTagDiv")
+            // hiddenTagDiv.style.display = "block" 
+        } else {
+            tagBtn.setAttribute("selected", "false")
+            // trouver autre chose 
+            let hiddenTagDiv = cards[i].querySelector(".hiddenTagDiv")
+            hiddenTagDiv.classList.add("hiddenTagDiv")
+        }
+       
+    })
+
+    selectedBtn.addEventListener("click", (e) => {
+        e.preventDefault()
+        let selected = selectedBtn.getAttribute("selected")
+        if (selected == "false") {
+            selected = null
+        }
+        if (!selected){
+            selectedBtn.setAttribute("selected", "true")
+            selectedBtn.classList.add("selectedHeart")
+        } else {
+            selectedBtn.setAttribute("selected", "false")
+            selectedBtn.classList.remove("selectedHeart")
+        }
+       
+    })
+
+
 }
