d94b3199e4e0f1c330fd395af1ac9232a0ce9f40
stupnikk <andre.lacassagne82@gmail.com>
2024-06-22 09:02:51 +0200

    ok

--- gstore/setup_test.go
+++ gstore/setup_test.go
@@ -1,12 +1,17 @@
 package gstore
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"os"
 	"testing"
 
+	"cloud.google.com/go/storage"
 	"github.com/joho/godotenv"
 )
+
+var TestBucketName = "someidealtestbucketname"
 
 func TestMain(m *testing.M) {
 
@@ -15,7 +20,32 @@ func TestMain(m *testing.M) {
 	}
 
 	code := m.Run()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	client, err := storage.NewClient(ctx)
 
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bucket := client.Bucket(TestBucketName)
+
+	bucketAttrs := &storage.BucketAttrs{
+		Location: "EU",
+	}
+
+	projectID := "zikstudio"
+	// Creates the new bucket.
+
+	if err := bucket.Create(ctx, projectID, bucketAttrs); err != nil {
+		log.Fatalf("Failed to create bucket: %v", err)
+	}
+
+	fmt.Print("Bucket created.\n")
+
+	defer func() {
+		client.Bucket(TestBucket).Delete(ctx)
+	}()
 	// Exit with the received code
 	os.Exit(code)
 }
