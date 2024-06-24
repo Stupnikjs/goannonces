85b5ecc8a9fc1bb0b2567c3de1f326529ba5375b
Stupnikjs <106406099+Stupnikjs@users.noreply.github.com>
2024-06-24 10:47:05 +0200

    Update db.go

--- repo/db.go
+++ repo/db.go
@@ -129,6 +129,19 @@ func (rep *PostgresRepo) UpdateTrackTag(trackId int32, tag string) error {
 // create route to increment PlayCount
 // selectcnt
 
+
+
+func (rep *PostgresRepo) CreatePlaylist(playlistName string) error {
+        ctx, cancel := context.WithCancel(context.Background())
+        defer cancel()
+
+                _, err := rep.DB.ExecContext(ctx, InsertPlaylistQuery, playlistName)
+                if err != nil {
+                        return err
+                }
+        }
+        return nil
+}
 func (rep *PostgresRepo) PushPlaylistItem(playlistName string, trackId int32) error {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
