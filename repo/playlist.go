14a953539b15dad39d9b845e12973ec244038b16
Stupnikjs <106406099+Stupnikjs@users.noreply.github.com>
2024-06-24 21:26:30 +0200

    Update playlist.go

--- repo/playlist.go
+++ repo/playlist.go
@@ -1,1 +1,30 @@
 package repo
+
+
+import (
+
+)
+
+
+func (rep *PostgresRepo) CreatePlaylist(playlistName string) error {
+        ctx, cancel := context.WithCancel(context.Background())
+        defer cancel()
+
+                _, err := rep.DB.ExecContext(ctx, InsertPlaylistQuery, playlistName)
+                if err != nil {
+                        return err
+                }
+        }
+        return nil
+}
+func (rep *PostgresRepo) PushPlaylistItem(playlistName string, trackId int32) error {
+        ctx, cancel := context.WithCancel(context.Background())
+        defer cancel()
+
+                _, err := rep.DB.ExecContext(ctx, InsertPlaylistTrackQuery, playlistName, trackid)
+                if err != nil {
+                        return err
+                }
+        }
+        return nil
+}
