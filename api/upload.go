46b365da7c5015f120186e2f7ffa7fe98aa8d2a9
Stupnikjs <106406099+Stupnikjs@users.noreply.github.com>
2024-07-06 13:22:24 +0200

    Update upload.go

--- api/upload.go
+++ api/upload.go
@@ -15,74 +15,67 @@ import (
 func ByteFromMegaFile(file io.Reader) ([]byte, error) {
 
 	reader := bufio.NewReader(file)
-
 	finalByteArr := make([]byte, 0, 2048*1000)
-
 	for {
 		soloByte, err := reader.ReadByte()
 		if err != nil {
 			log.Println(err)
 			break
 		}
-
 		finalByteArr = append(finalByteArr, soloByte)
 	}
-
 	return finalByteArr, nil
-
 }
+
+
 
 func (app *Application) LoadMultipartReqToBucket(r *http.Request, bucketName string) (string, error) {
 	objNames, err := gstore.ListObjectsBucket(app.BucketName)
-
+	
+	// filename with already present storage object names 
 	already := []string{}
 	if err != nil {
 		return "", err
 	}
 
+	fmt.Println(r.MultipartForm.File)
+	
 	for _, headers := range r.MultipartForm.File {
 
 		for _, h := range headers {
-
 			if util.IsInSlice(h.Filename, objNames) {
-				// format a messgage with already present files
 				already = append(already, h.Filename)
-
 				break
 			}
 
 			file, err := h.Open()
-
 			if err != nil {
 				return "", err
 			}
-
 			defer file.Close()
 
 			finalByteArr, err := ByteFromMegaFile(file)
-
 			if err != nil {
 				return "", err
 			}
 
 			err = gstore.LoadToBucket(bucketName, h.Filename, finalByteArr)
-
 			if err != nil {
 				return "", err
 			}
 
 			track := repo.Track{}
 			url, err := gstore.GetObjectURL(bucketName, h.Filename)
-
 			if err != nil {
 				return "", err
 			}
-
 			track.StoreURL = url
 			track.Name = h.Filename
-			track.Selected = false
 			err = app.DB.PushTrackToSQL(track)
+			
 			if err != nil {
+				fmt.Println("error appened pushing track to sql after pushing to storage, so deleting object in strorage")
+				errDelete := gstore.DeleteObject()
 				return "", err
 			}
 		}
