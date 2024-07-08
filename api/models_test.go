281cc52ac1a526b6bff0152ebead2ccbe1845d4f
Stupnikjs <106406099+Stupnikjs@users.noreply.github.com>
2024-07-08 13:18:56 +0200

    Update models_test.go

--- api/models_test.go
+++ api/models_test.go
@@ -8,8 +8,20 @@ import (
 
 
 func TestParseJsonReq(t *testing.T){
+    // create mockrequest 
+    req := httptest.NewRequest() 
+    /* type POST
+    
+             {
+                "action": "update",
+                "object": {
+                    "type" : "track",
+                    "id": trackid,
+                    "field" : "tag",
+                    "body": input.value
+                }
+            } 
 
-
-
-
-  }
+     */
+    }
+  
