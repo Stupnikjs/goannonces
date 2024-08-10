374b7062eada1bf2c259c09f9572f97c17c4f71a
Stupnikjs <n.boudier.ph@gmail.com>
2024-08-10 15:30:48 +0200

    ok

--- api/models.go
+++ api/models.go
@@ -1,6 +1,10 @@
 package api
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	"strconv"
+)
 
 type Application struct {
 	Port int
@@ -8,12 +12,77 @@ type Application struct {
 }
 
 type Annonce struct {
+	Id          string `json:"id"`
 	Url         string `json:"url"`
 	PubDate     string `json:"pubdate"`
+	Ville       string `json:"ville"`
 	Lieu        string `json:"lieu"`
-	Region      string `json:"region"`
 	Departement int    `json:"departement"`
 	Description string `json:"description"`
 	Profession  string `json:"profession"`
 	Contrat     string `json:"contrat"`
+	Created_at  string `json:"created_at"`
+}
+
+func AnnonceToHtml(a Annonce) string {
+
+	str := fmt.Sprintf(`
+		<a class="annonceLink" href="%s">
+	 		<span class="depSpan">%d</span>
+			<span class="lieuSpan">%s</span>
+		</a>
+
+	`, a.Url, a.Departement, a.Lieu)
+
+	return str
+}
+
+func MapContains(m map[string][]string, key string) bool {
+	v, exists := m[key]
+	if v[0] != "" {
+		return true
+	}
+	return exists
+}
+
+func GetFilteredAnnonces(form map[string][]string) []Annonce {
+	filteredAnnonces := []Annonce{}
+	annonces, _ := LoadAnnonces()
+
+	// Check if required keys are present in the form
+	job := MapContains(form, "job")
+	dep := MapContains(form, "dep")
+	contract := MapContains(form, "contract")
+
+	fmt.Println(job, dep, contract)
+	for _, an := range annonces {
+		// Assume that all criteria are required to match if they are provided
+		matches := true
+
+		// Apply filters based on presence in the form
+		if job {
+			if an.Profession != form["job"][0] {
+				matches = false
+			}
+		}
+		if dep {
+			// map over
+			depNum, err := strconv.Atoi(form["dep"][0])
+			if err != nil || an.Departement != depNum {
+				matches = false
+			}
+		}
+		if contract {
+			if an.Contrat != form["contract"][0] {
+				matches = false
+			}
+		}
+
+		// If all criteria are satisfied, append the annonce
+		if matches {
+			filteredAnnonces = append(filteredAnnonces, an)
+		}
+	}
+
+	return filteredAnnonces
 }
--- api/routes.go
+++ api/routes.go
@@ -12,7 +12,7 @@ func (app *Application) Routes() http.Handler {
 
 	// register routes
 	mux.Get("/", app.RenderAccueil)
-
+	mux.Post("/api/annonces", app.GetHTMLAnnonces)
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
